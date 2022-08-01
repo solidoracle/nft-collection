4f7767cf29cd305bb2fee6f7223b68a34b58b62d
solidoracle <solidoracle17@gmail.com>
2022-08-01 09:18:17 +0200

    deployment complete

---
+++ hardhat-tutorial/contracts/IWhitelist.sol
@@ -0,0 +1,16 @@
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.4;
+
+
+
+    /*
+    Notice: As we only need to call mapping(address => bool) public whitelistedAddresses; 
+    We can create an interface for Whitelist contract with a function only for this mapping, 
+    this way we would save gas as we would not need to inherit and deploy the entire Whitelist Contract but only a part of it.
+    */ 
+
+
+
+ interface IWhitelist {
+     function whitelistedAddresses(address) external view returns (bool);
+ }
