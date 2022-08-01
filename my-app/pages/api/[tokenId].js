2731a90c968b86e80e98067a371270799506c112
solidoracle <solidoracle17@gmail.com>
2022-08-01 11:24:53 +0200

    token id api

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
       "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
-    // The api is sending back metadata for a Crypto Dev
+    // The api is sending back metadata for a Galactic Ape
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
     // More info can be found here: https://docs.opensea.io/docs/metadata-standards
